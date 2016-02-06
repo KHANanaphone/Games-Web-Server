178305df6cc236946a1832929ed5c22418d1bf22
Adan Ferguson <adanferguson@gmail.com>
2016-02-05 18:24:31 -0700

    styling changes

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,9 +1,9 @@
 load('application');
 
-action('index', function(){
-    
+action('blog', function(){
+
     var page = req.param('id') ? req.param('id') : 0;
-    
+
     render({
         title: "Adan's Game Design",
         page: page
@@ -11,30 +11,30 @@ action('index', function(){
 });
 
 action('games', function(){
-    
+
     render({
         title: "Adan's Game Design - Games"
            });
 });
 
 action('about', function(){
-    
+
     render({
         title: "Adan's Game Design - About"
            });
 });
 
 action('login', function(){
-    
+
     var code = req.param('code');
-    
+
     if(code == 'koolkode420'){
-        
+
         req.session.authed = true;
         redirect('/posts');
     }
-    
+
     render({
         title: "Adan's Game Design - About"
-           }); 
+           });
 });
--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,17 @@
 exports.routes = function (map) {
-    
+
 
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
-    
-    map.get('/', 'home#index');
-    map.get('/page', 'home#index');
+
+    map.get('/', 'home#games');
+    map.get('/blog', 'home#blog');
     map.get('/games', 'home#games');
     map.get('/about', 'home#about');
     map.get('/login', 'home#login');
-    
+
     map.get('/posts', 'posts#list');
-    
+
 };
